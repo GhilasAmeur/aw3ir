87da1cbc954877ce8ec67eaf4ebdd81a138f80f7
GhilasAmeur <ghilasameur@yahoo.com>
2019-11-11 21:33:46 +0100

    www

--- tp2/index_essai.js
+++ tp2/index_essai.js
@@ -1,10 +1,14 @@
 document.getElementById("validation").addEventListener("submit", function(e){
+  
     //var erreur = document.getElementById("erreur");
    // e.preventDefault();
 
-    var nom = document.getElementById("nom");
+
+   // ca marche bien pour le nom 
+  /*  var nom = document.getElementById("nom");
     var  erreur = document.getElementById("erreur");
-    var inputs = document.getElementById("input");
+  
+
     if(!nom.value){
        
            e.preventDefault();
@@ -18,11 +22,49 @@ document.getElementById("validation").addEventListener("submit", function(e){
 
    }else{
        alert("formulaire envoyéé !!")
-   }
-    /* else if(nom.value < 5) {
-        erreur = "Saisi au moins 5 caractères !" ;
-        console.log("erreur") ;
-    }else{
-        console.log("Formulaire bien envoyé !!")
-    }*/
+   } ca marche bien pour le nom fini ici */
+
+   
+
+   //boucle for pour tous les champs commence ici recuperer les inputs  !! 
+
+var nom = document.getElementById("nom");
+var prenom = document.getElementById("prenom");
+var date = document.getElementById("date");
+var adresse = document.getElementById("adresse");
+var email = document.getElementById("email");
+var erreur ;
+
+if(!nom.value | nom.value.length<5){
+    
+    erreur = "Renseignez un nom qui contient au moins 5 caractères !";
+
+}
+if(!prenom.value | prenom.value.length<5){
+    
+    erreur = "Renseignez un prénom qui contient au moins 5 caractères !";
+
+}
+if(!date.value | date.value.length<5){
+    
+    erreur = "Renseignez une date valide";
+
+}
+if(!adresse.value | adresse.value.length<5){
+    
+    erreur = "Renseignez une adresse valide !";
+
+}
+if(!email.value | email.value.length<5){
+    
+    erreur = "Renseignez un Email valide !";
+
+}
+if(erreur){
+    e.preventDefault();
+    document.getElementById("erreur").innerHTML = erreur ;
+}else{
+    e.preventDefault();
+    document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#nom").value ;
+}
 })
