940e83c937528d7e88d0ee6028979a458a8b92c7
GhilasAmeur <ghilasameur@yahoo.com>
2019-11-18 14:57:49 +0100

    cc,dd
    hghghghh

---
+++ tp3/js/form-jquery-validation.js
@@ -0,0 +1,16 @@
+
+
+
+ $(document).ready(function () {
+    ​
+    var resultat = true ;
+        // interception du "submit" du formulaire
+        $("form").on("submit", function (event) {
+            event.preventDefault();
+            // votre code de vérification du formulaire
+    ​       if($('#nom').val()==""){
+        
+    }
+    ​
+        });
+    });
