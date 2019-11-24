498f1fc1855d564dfbfb4654c4bf1ad954e539fc
GhilasAmeur <ghilasameur@yahoo.com>
2019-11-24 21:40:10 +0100

    nnnn
    nnn

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -1,3 +1,87 @@
+/*
+
+$( document ).ready(function() {
+    $( "#datepicker-wdt" ).datepicker({dateFormat: 'dd/mm/yy', maxDate:new Date()});
+     //$( "#datepicker-wdt" ).datepicker("show");
+   
+     });
+   
+   function validation(){
+      
+         
+   
+   
+       var error = "";
+       var nom=$("#nom").val(); 
+       var prenom=$("#prenom").val(); 
+       var date_de_naissance=$("#datepicker-wdt").val(); 
+       var adresse=$("#adresse").val(); 
+       var mail=$("#mail").val(); 
+   
+   
+           if(nom.length < 5) {
+              error += "nom, ";
+           }
+           if(prenom.length < 5) {
+              error += "prenom, ";
+           }
+           if(date_de_naissance.length < 5) {
+              error += "date_de_naissance, ";
+           }
+           if(adresse.length < 5) {
+              error += "adresse, ";
+           }
+           if(mail.length < 5) {
+              error += "mail, ";
+           }
+   
+   if(error == ""){
+           $(".modal-title").text('Bienvenue ' + nom);
+           $(".modal-body").html('vous êtes nés le '  + date_de_naissance+ ' et vous habitez : ' + '<img src="https://maps.googleapis.com/maps/api/staticmap?markers=' + adresse + '&zoom=14&size=150x150&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg"> </img> </br>' + adresse);
+   }
+   else{
+           $(".modal-title").text('Message Modal');
+           $(".modal-body").html('les champs '+error + ' doivent être remplis');
+   }
+   
+       $('#myModal').modal("show");
+   
+   }
+   */
+   
+
+
+
+
+  $( document ).ready(function() {
+    $( "#datepicker-wdt" ).datepicker({dateFormat: 'dd/mm/yy', maxDate:new Date()});
+     //$( "#datepicker-wdt" ).datepicker("show");
+   
+     });
+
+     function validation(){
+
+        var error = "" ;
+        var nom = $("#nom").val() ;
+        if(nom.val()== "" || nom.length < 5 ){
+            
+
+        }
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //$( document ).ready(function() {
